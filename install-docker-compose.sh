f8b043deae42d40c9714f0438a6eac0e5c34fb61
Samuel Stevens <samuel.stevens.2@us.af.mil>
2021-10-12 18:39:40 -0400

    more arm64 stuff

--- install-docker-compose.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -L https://github.com/linuxserver/docker-docker-compose/releases/download/1.28.5-ls32/docker-compose-armhf | sudo tee /usr/local/bin/docker-compose >/dev/null
---
+++ install_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+copy_chronograf.sh
+copy_grafana.sh
+copy_influxdb.sh
+copy_kapacitor.sh
+copy_nodered.sh
+copy_telegraf.sh
